731793a2c443fc376c1e77c7478b6ad37009a3d4
samKit88 <samuel.girma.cx9896@gmail.com>
2024-10-08 16:12:23 +0300

    add feature on inventory service

--- src/inventory/inventory.service.ts
+++ src/inventory/inventory.service.ts
@@ -3,18 +3,27 @@ import { PrismaService } from 'src/prisma/prisma.service';
 import { InveDto } from './dto/inve.dto';
 import { CategoryDto } from './dto/category.dto';
 import { BrandDto } from './dto/brand.dto';
+import { QueryDto } from './dto/query.dto';
+import * as fs from 'fs';
+import * as path from 'path';
 
 @Injectable()
 export class InventoryService {
   constructor(private prismaService: PrismaService) {}
-  async getAll() {
-    const productes = await this.prismaService.inventory.findFirst();
 
-    console.log(productes);
+  async getAll(page: number, limit: number, queryDto: QueryDto) {
+    const productes = await this.prismaService.inventory.findMany({
+      skip: page,
+      take: limit,
+      where: { name: { contains: queryDto.search } },
+      orderBy: { [queryDto.sort]: queryDto.order },
+    });
+
+    // console.log(productes);
 
     if (!productes) return 'No producte';
 
-    return productes.name;
+    return productes;
   }
 
   async getByID(reqId: number) {
@@ -169,5 +178,77 @@ export class InventoryService {
     });
 
     return deleteInve.name;
+  }
+
+  async uploadImage(inventoryId: number, fileUrl: string) {
+    const producte = await this.prismaService.inventory.findUnique({
+      where: {
+        id: inventoryId,
+      },
+    });
+
+    if (!producte) throw new ForbiddenException('No Producte');
+
+    const countImage = await this.prismaService.image.count({
+      where: {
+        imageId: producte.id,
+      },
+    });
+
+    if (countImage >= 5)
+      throw new ForbiddenException('Can not add another image');
+
+    const image = await this.prismaService.image.create({
+      data: {
+        imageURL: fileUrl,
+        imageId: producte.id,
+      },
+    });
+
+    return `Image saved successfuly! ${image}`;
+  }
+
+  async getImageById(inventoryId: number) {
+    const producte = await this.prismaService.inventory.findUnique({
+      where: {
+        id: inventoryId,
+      },
+    });
+
+    if (!producte) throw new ForbiddenException('No Producte!!!');
+
+    const producteImages = await this.prismaService.image.findMany({
+      where: {
+        imageId: producte.id,
+      },
+    });
+    return producteImages;
+  }
+
+  async deleteImage(inventoryId: number, deleteImageId: number) {
+    const producte = await this.prismaService.inventory.findUnique({
+      where: {
+        id: inventoryId,
+      },
+    });
+
+    if (!producte) throw new ForbiddenException('No Producte');
+
+    const imageDeleted = await this.prismaService.image.delete({
+      where: {
+        id: deleteImageId,
+        imageId: producte.id,
+      },
+    });
+
+    const filePath = path.join(imageDeleted.imageURL);
+
+    fs.unlink(filePath, (err) => {
+      if (err) {
+        console.log(`Error deleting file ${err}`);
+      }
+    });
+
+    return imageDeleted;
   }
 }
