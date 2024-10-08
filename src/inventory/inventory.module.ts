9262caf956634aabbe3abd8e47b1e59494454725
samKit88 <samuel.girma.cx9896@gmail.com>
2024-10-08 16:09:33 +0300

    add feature Multer module

--- src/inventory/inventory.module.ts
+++ src/inventory/inventory.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { InventoryService } from './inventory.service';
 import { InventoryController } from './inventory.controller';
+import { MulterModule } from '@nestjs/platform-express';
 
 @Module({
+  imports: [
+    MulterModule.register({
+      dest: './uploads',
+    }),
+  ],
   providers: [InventoryService],
-  controllers: [InventoryController]
+  controllers: [InventoryController],
 })
 export class InventoryModule {}
