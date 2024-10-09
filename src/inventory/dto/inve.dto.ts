778db532c63f198866319d4b98a651cbe554cc09
samKit88 <samuel.girma.cx9896@gmail.com>
2024-10-09 13:37:41 +0300

    add feature validator for inventory dto

--- src/inventory/dto/inve.dto.ts
+++ src/inventory/dto/inve.dto.ts
@@ -1,30 +1,27 @@
-import { IsString } from 'class-validator';
+import { IsDecimal, IsEnum, IsNumber, IsString } from 'class-validator';
 import { Product, Tax, Unit } from '@prisma/client';
 
 export class InveDto {
   @IsString()
   name: string;
-
-  userID: number;
-
   @IsString()
   barcode: string;
-
-  category: number;
-
-  brand: number;
-
+  @IsNumber()
+  categoryId: number;
+  @IsNumber()
+  brandId: number;
+  @IsDecimal()
   buyingPrice: number;
-
+  @IsDecimal()
   sellingPrice: number;
-
+  @IsEnum(Unit)
   productUnit: Unit;
-
+  @IsNumber()
   quantity: number;
-
+  @IsEnum(Tax)
   taxType: Tax;
-
+  @IsString()
   description: string;
-
+  @IsEnum(Product)
   produtType: Product;
 }
